7f7a8068a00470dfca04427dc30630e0dc448042
Breeeeze <1578363627@qq.com>
2025-03-20 17:31:09 +0800

    4

---
+++ HW4/1/Program.cs
@@ -0,0 +1,76 @@
+﻿using static System.Net.Mime.MediaTypeNames;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Xml.Linq;
+using System;
+using System.Runtime.InteropServices;
+
+namespace _1
+{
+    // 链表节点
+    public class Node<T>
+    {
+        public Node<T> Next { get; set; }
+        public T Data{ get; set; }
+        public Node(T t)
+        {
+            Next = null;
+            Data = t;
+        }
+    }
+    //泛型链表类
+    public class GenericList<T>
+    {
+        private Node<T> head; private Node<T> tail;
+        public GenericList()
+        {
+            tail = head = null;
+        }
+        public Node<T> Head
+        {
+            get => head;
+        }
+        public void Add(T t)
+        {
+            Node<T> n = new Node<T>(t);
+            if (tail == null)
+            {
+                head = tail = n;
+            }
+            else
+            {
+                tail.Next = n; tail = n;
+            }
+        }
+        public void ForEach(Action<T> action)
+        {
+            for(Node<T> n = head; n != null; n = n.Next)
+            {
+                action(n.Data);
+            }
+        }
+    }
+    //字符串型List
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            GenericList<int> intlist = new GenericList<int>();
+            for (int x = 0; x < 10; x++)
+            {
+                intlist.Add(x);
+            }
+            int max=-999, min=999, sum=0;
+            Console.WriteLine("遍历元素:");
+            intlist.ForEach((int x) => { Console.WriteLine(x); });
+            intlist.ForEach((int x) => { if (x > max) max = x; });
+            intlist.ForEach((int x) => { if (x < min) min = x; });
+            intlist.ForEach((int x) => { sum += x; });
+            Console.WriteLine("");
+            Console.WriteLine($"最大值:{max}");
+            Console.WriteLine($"最小值:{min}");
+            Console.WriteLine($"总和:{sum}");
+        }
+    }
+}
---
+++ HW4/2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Threading;
+
+// 定义闹钟类
+public class AlarmClock
+{
+    public delegate void alarmm();
+    // 定义 Tick 事件
+    public event alarmm Tick;
+    // 定义 Alarm 事件
+    public event alarmm Alarm;
+
+    // 闹钟的响铃时间（秒）
+    public int AlarmTime { get; set; }
+
+    // 启动闹钟
+    public void Start()
+    {
+        for (int i = 1; i <= AlarmTime; i++)
+        {
+            // 触发 Tick 事件
+            Tick();
+            Thread.Sleep(1000);  // 模拟 1 秒钟的嘀嗒
+        }
+
+        // 触发 Alarm 事件
+        Alarm();
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // 创建闹钟实例
+        AlarmClock clock = new AlarmClock();
+        clock.AlarmTime = Console.Read()-(int)'0';  // 设置闹钟响铃时间为 10 秒
+
+        // 订阅 Tick 事件
+        clock.Tick += () => Console.WriteLine("Tick...");
+
+        // 订阅 Alarm 事件
+        clock.Alarm += () => Console.WriteLine("Alarm! Wake up!");
+
+        // 启动闹钟
+        Console.WriteLine($"闹钟启动，将在 {clock.AlarmTime} 秒后响铃...");
+        clock.Start();
+    }
+}
